b0159cbe945936d4fad961e740f460b8db3f2a2c
Irakli Safareli <i.safareli@gmail.com>
2018-01-13 15:43:11 +0100

    add stacksafe version of FreeAp
    from https://github.com/ethul/purescript-freeap/pull/13

--- src/Main.purs
+++ src/Main.purs
@@ -20,6 +20,8 @@ import FreeAp9ccd7db as FreeApOld
 
 import FreeAp71acf67 as FreeApNew
 
+import FreeApStackSafe as FreeApStackSafe
+
 import FreeApDay as FreeApDay
 
 trials :: Int
@@ -27,7 +29,7 @@ trials = 1
 
 benchRetractFreeAp :: Int -> String -> Benchmark
 benchRetractFreeAp size label = mkBenchmark
-  { slug: "retractFreeAp-71acf67-9ccd7db-day-" <> toLower label
+  { slug: "retractFreeAp-71acf67-9ccd7db-day-safe-" <> toLower label
   , title: "retractFreeAp (sequence [ FreeAp f a, ..., FreeAp f z ]) - " <> label
   , sizes: (1..20) <#> (_ * size)
   , sizeInterpretation: "Number of elements in the array"
@@ -35,38 +37,48 @@ benchRetractFreeAp size label = mkBenchmark
   , gen: \n -> { old: _
                , new: _
                , day: _
+               , safe: _
                } <$> vectorOf n (pure (FreeApOld.liftFreeAp (Identity unit)))
                  <*> vectorOf n (pure (FreeApNew.liftFreeAp (Identity unit)))
                  <*> vectorOf n (pure (FreeApDay.liftFreeAp (Identity unit)))
-  , functions: [ benchFn "FreeAp Old @ 9ccd7db" (\a -> FreeApOld.retractFreeAp (sequence a.old))
-               , benchFn "FreeAp New @ 71acf67" (\a -> FreeApNew.retractFreeAp (sequence a.new))
+                 <*> vectorOf n (pure (FreeApStackSafe.liftFreeAp (Identity unit)))
+  , functions: [ 
+    -- benchFn "FreeAp Old @ 9ccd7db" (\a -> FreeApOld.retractFreeAp (sequence a.old))
+              --  , 
+               benchFn "FreeAp New @ 71acf67" (\a -> FreeApNew.retractFreeAp (sequence a.new))
                , benchFn "FreeAp Day" (\a -> FreeApDay.retractFreeAp (sequence a.day))
+               , benchFn "FreeAp stacksafe" (\a -> FreeApStackSafe.retractFreeAp (sequence a.safe))
                ]
   }
 
 type A = { old :: Array (FreeApOld.FreeAp Identity Unit)
          , new :: Array (FreeApNew.FreeAp Identity Unit)
          , day :: Array (FreeApDay.FreeAp Identity Unit)
+         , safe :: Array (FreeApStackSafe.FreeAp Identity Unit)
          }
 
 benchFoldFreeAp :: Int -> String -> Benchmark
 benchFoldFreeAp size label = mkBenchmark
-  { slug: "foldFreeAp-71acf67-9ccd7db-day-" <> toLower label
+  { slug: "foldFreeAp-71acf67-9ccd7db-day-safe-" <> toLower label
   , title: "foldFreeAp (a -> a) (k <$> FreeAp f a <* FreeAp f b <* ... <* FreeAp f z) - " <> label
-  , sizes: (1..12) <#> (_ * size)
+  , sizes: (1..2) <#> (_ * size)
   , sizeInterpretation: "Number of elements in the array"
   , inputsPerSize: trials
   , gen: \n -> { old: _
                , new: _
                , day: _
+               , safe: _
                } <$> vectorOf n (pure (FreeApOld.liftFreeAp (Identity unit)))
                  <*> vectorOf n (pure (FreeApNew.liftFreeAp (Identity unit)))
                  <*> vectorOf n (pure (FreeApDay.liftFreeAp (Identity unit)))
-  , functions: [ benchFn "FreeAp Old @ 9ccd7db" (\(a :: A) ->
-                   maybe (pure unit)
-                         (\{head, tail} -> FreeApOld.foldFreeAp (\a' -> a') (id <$> (foldl (\x y -> x <* y) head tail)))
-                         (uncons a.old))
-               , benchFn "FreeAp New @ 71acf67" (\(a :: A) ->
+                 <*> vectorOf n (pure (FreeApStackSafe.liftFreeAp (Identity unit)))
+  , functions: [ 
+      -- benchFn "FreeAp Old @ 9ccd7db" (\(a :: A) ->
+      --              maybe (pure unit)
+      --                    (\{head, tail} -> FreeApOld.foldFreeAp (\a' -> a') (id <$> (foldl (\x y -> x <* y) head tail)))
+      --                    (uncons a.old))
+              --  , 
+               benchFn "FreeAp New @ 71acf67" (\(a :: A) ->
                    maybe (pure unit)
                          (\{head, tail} -> FreeApNew.foldFreeAp (\a' -> a') (id <$> (foldl (\x y -> x <* y) head tail)))
                          (uncons a.new))
@@ -74,12 +86,18 @@ benchFoldFreeAp size label = mkBenchmark
                    maybe (pure unit)
                          (\{head, tail} -> FreeApDay.foldFreeAp (\a' -> a') (id <$> (foldl (\x y -> x <* y) head tail)))
                          (uncons a.day))
+               , benchFn "FreeAp stacksafe" (\(a :: A) ->
+                   maybe (pure unit)
+                         (\{head, tail} -> FreeApStackSafe.foldFreeAp (\a' -> a') (id <$> (foldl (\x y -> x <* y) head tail)))
+                         (uncons a.safe))
                ]
   }
 
 main :: forall eff. Eff (BenchEffects eff) Unit
-main = runSuite [ benchRetractFreeAp 2 "Small"
-                , benchFoldFreeAp 1 "Small"
-                , benchRetractFreeAp 100 "Large"
-                , benchFoldFreeAp 10 "Large"
+main = runSuite [ 
+  -- benchRetractFreeAp 2 "Small"
+  --               , benchFoldFreeAp 1 "Small"
+                -- , 
+                benchRetractFreeAp 100 "Large"
+                , benchFoldFreeAp 2000 "Large"
                 ]
